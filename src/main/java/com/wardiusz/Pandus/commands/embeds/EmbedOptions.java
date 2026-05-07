0d5bfc511aa864188efcbb99784a3ecb91a4ac6d
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2026-05-07 18:34:52 +0200

    style: change neutral color

--- src/main/java/com/wardiusz/Pandus/commands/embeds/EmbedOptions.java
+++ src/main/java/com/wardiusz/Pandus/commands/embeds/EmbedOptions.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class EmbedOptions {
     public static final Color SUCCESS_COLOR = new Color(97, 178, 129);
     public static final Color ERROR_COLOR = new Color(220, 84, 68);
-    public static final Color NEUTRAL_COLOR = new Color(252, 249, 32);
+    public static final Color NEUTRAL_COLOR = new Color(255, 255, 255);
     public static final Emoji SUCCESS_EMOJI = Emoji.fromFormatted("<:success:1115778398212591676>");
     public static final Emoji FAIL_EMOJI = Emoji.fromFormatted("<:fail:1115782788227010610>");
     public static final Emoji NEUTRAL_EMOJI = Emoji.fromFormatted("<:tick:1242118679613935696>");
